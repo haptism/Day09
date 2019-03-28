02b74b21afb0d78772a663824a6d6519f6126b08
{haptism} <haptism007@gmail.com>
2019-03-28 09:26:28 +0900

    Initial commit

---
+++ src/com/iu/overloading/Cal.java
@@ -0,0 +1,11 @@
+package com.iu.overloading;
+
+// 계산 하는 클래스
+public class Cal {
+
+	public int plus(int a, int b) { // 두개를 더할려고 하는것
+		int sum = a + b;
+		return sum;
+	}
+
+}
---
+++ src/com/iu/overloading/OverMain.java
@@ -0,0 +1,10 @@
+package com.iu.overloading;
+
+public class OverMain {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
